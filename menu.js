37faf78718895548f5098ae19bd6ef1c6d83a093
Rikad Karlsson <git@rikardkarlsson.se>
2017-01-16 23:27:48 +0100

    bättre struktur och fler kommentarer

--- menu.js
+++ menu.js
@@ -1,18 +1,25 @@
 "use strict"; //use a better sub-part of JavaScript
 //uses JQuery
+
+//Is called when the Document Object Model (DOM) is fully loaded.
 $(document).ready(function(){ 
 	//print debug info to console window
 	console.log("starting script");
-     var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
-     //loop through all a-tags one at the time
+
+     //variables
+     var url = window.location.href;
+     var indexOfLastSlash = url.lastIndexOf("/");
+     var urlAfterSlash = url.substr(indexOfLastSlash + 1);
+
+     //loop through all a-tags within a nav-tag, one at the time
      $("nav a").each(function(){
-     	  //compare the part of the url after the last /, with href in the a-tag.
-     	  //if equal add class="selected" to this a-tag.
-          if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+     	//compare the part of the url after the last /, with href in the a-tag.
+          if($(this).attr("href") == urlAfterSlash || $(this).attr("href") == '' )
           {
+               //if equal add class="selected" to this a-tag.
           	$(this).addClass("selected");
           }
           //print debug info to console window
-          console.log("url: " + pgurl);        
+          console.log("url after slash: " + urlAfterSlash);        
      })
 });
